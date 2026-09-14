ed7ea13746a42e69ac1895fa1e427099af313450
Nishant7819 <nc1378569@gmail.com>
2026-09-14 14:50:30 +0530

    Pivot index solution

---
+++ pivotIndex.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int  n = nums.length;
+        int[]pre = new int[n];
+        pre[0] = nums[0];
+        for(int i = 1;i<n;i++){
+            pre[i] = pre[i-1]+nums[i];
+        }
+
+
+       for(int i =0;i<n;i++){
+        int ls = (i==0) ? 0 :pre[i-1];
+        int rs  = pre[n-1]-pre[i];
+        if(ls == rs){
+            return i;
+        }
+       }
+       return -1;
+    }
+}
